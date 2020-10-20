da15d4af78dc2bf015d332aa59b39e97e72579bb
Aaron Amankwaah <aaronamankwaah@nwk-80-251.nwk.ruw.rutgers.edu>
2020-10-20 17:22:11 -0400

    HW2

--- HW1-V2/ShapeTester/Shape.java
+++ HW1-V2/ShapeTester/Shape.java
@@ -1,5 +1,1 @@
 
-public abstract class Shape{
-    abstract public double computeArea();
-    abstract public double computePerimeter();
-    }
--- HW2/HW2_Q1.java
+++
@@ -1,7 +0,0 @@
-
-
-public class HW2_Q1{
-    public static void main(String[] args) {
-    System.out.println("Q1!!");
-    }
-}
